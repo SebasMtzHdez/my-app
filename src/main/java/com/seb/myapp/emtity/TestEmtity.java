02a2fb5f1e55e1769560104e867e93c35e1204e9
SebasMtzHdez <sebas_godofwar16@hotmail.com>
2023-02-08 19:37:57 -0600

    Nombre del Proyecto: my-app. Description: Version final del proyecto my-app

---
+++ src/main/java/com/seb/myapp/emtity/TestEmtity.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.seb.myapp.emtity;
+
+/**
+ * @author Sebastian
+ *
+ */
+public class TestEmtity {
+
+	/*
+	 * Propiedad de prueba para la muestra de compilacion del archivo testtentity
+	 */
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+}
